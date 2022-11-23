f9d5859d816c11f1288e3fecc84da44e5ec5ca93
luigicr95 <luigicr95@gmail.com>
2022-11-23 16:48:25 +0100

    Aggiungere i file di progetto.

---
+++ U1-W1-D3-BACK-ESERCIZIO/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U1_W1_D3_BACK_ESERCIZIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContoCorrente CC = new ContoCorrente();
+
+            string scelta = "";
+
+            do
+            {
+                Console.WriteLine("Seleziona la scelta");
+                Console.WriteLine("1. APRI CONTO CORRENTE");
+                Console.WriteLine("2. PRELIEVO");
+                Console.WriteLine("3. VERSAMENTO");
+                scelta = Console.ReadLine();
+                if (scelta == "1")
+                {
+                    Console.WriteLine("APERTURA CONTO CORRENTE");
+                    CC.Apertura = true;
+                    Console.WriteLine("Nome Intestatario:");
+                    CC.NomeIntestatario = Console.ReadLine();
+                    Console.WriteLine("Cognome Intestatario:");
+                    CC.CognomeIntestatario = Console.ReadLine();
+                    Console.WriteLine($"Conto Corrente di {CC.NomeIntestatario} {CC.CognomeIntestatario} ID: 22234451 aperto correttamente! \r\n" +
+                        $"E' necessario effettuare un versamento iniziale di almeno 1000 euro \r\n" +
+                        $"Digitare l'importo:");
+                    int cifraVersata = int.Parse(Console.ReadLine());
+                    CC.VersamentoIniziale(cifraVersata);
+
+                }
+                else if (scelta == "2")
+                {
+                    if (CC.Apertura == false)
+                    {
+                        Console.WriteLine("Devi aprire un conto per effettuare un prelievo!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("PRELIEVO");
+                        Console.WriteLine($"Saldo attuale: {CC.Saldo}");
+                        Console.WriteLine("Inserire l'importo da prelevare:");
+                        int cifraPrelevata = int.Parse(Console.ReadLine());
+                        CC.Prelievo(cifraPrelevata);
+                        Console.WriteLine($"Saldo attuale: {CC.Saldo}");
+                    }
+                }
+                else if (scelta == "3")
+                {
+                    if (CC.Apertura == false)
+                    {
+                        Console.WriteLine("Devi aprire un conto per effettuare un versamento!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("VERSAMENTO");
+                        Console.WriteLine($"Saldo attuale: {CC.Saldo}");
+                        Console.WriteLine("Inserire l'importo da versare" +
+                            ":");
+                        int cifraVersata = int.Parse(Console.ReadLine());
+                        CC.Versamento(cifraVersata);
+                        Console.WriteLine($"Saldo attuale: {CC.Saldo}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Scelta non valida");
+                }
+            } while (scelta != "1" | scelta != "2" | scelta != "3");
+
+
+            //switch (scelta)
+            //{
+            //    case "1":
+            //        Console.WriteLine("Apertura Conto Corrente");
+            //        break;
+            //    case "2":
+            //        Console.WriteLine("Prelievo");
+            //        break;
+            //    case "3":
+            //        Console.WriteLine("Versamento");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Scelta non valida");
+            //        break;
+            //}
+
+        
+
+            Console.ReadLine();
+
+
+        }
+
+        public class ContoCorrente
+        {
+            public string NomeIntestatario { get; set; }
+            public string CognomeIntestatario { get; set; }
+            public int Saldo { get; set; } = 0;
+            public bool Apertura { get; set; } = false;
+
+            public void Prelievo (int cifra)
+            {
+                if (Saldo < cifra)
+                {
+                    Console.WriteLine("Saldo disponibile insufficiente!");
+                }
+                else
+                {
+                    Saldo = Saldo - cifra;
+                }
+                
+            }
+            public void Versamento (int cifra)
+            {
+                Saldo = Saldo + cifra;
+            }
+
+            public void VersamentoIniziale(int cifra)
+            {
+                if (cifra < 1000)
+                {
+                    Console.WriteLine("Il versamento deve essere pari ad almeno 1000 euro");
+                }
+                else
+                {
+                    Saldo = Saldo + cifra;
+                }
+            }
+        }
+    }
+}
---
+++ U1-W1-D3-BACK-ESERCIZIO/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("U1-W1-D3-BACK-ESERCIZIO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("U1-W1-D3-BACK-ESERCIZIO")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("c8e543a8-5867-4d03-9539-10407cd9352d")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
